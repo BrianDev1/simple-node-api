39289ea761790608dd3dd2c30bfa5b5229864e20
Brian Rudolph <brian.rudolph303@gmail.com>
2021-09-13 11:48:44 -0600

    fix: added comment to header setting

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const app = express();
 
 app.use(bodyParser.json());
 
+// Setting public and allowing access
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
+
 app.use("/feed", feedRoutes);
 
 
